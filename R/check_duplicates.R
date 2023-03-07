52b0650baf4977554a20ea3b80a5f70271d60d4b
PorwalS <shaun.porwal@gmail.com>
2023-03-07 15:29:19 -0500

    Fixing devtools::check() issues

--- R/check_duplicates.R
+++ R/check_duplicates.R
@@ -26,6 +26,8 @@
 #' # Call the function to check for duplicates within the "ID" and "Age" columns
 #' check_duplicates(data_df, group_by_vars = c("ID", "Age"))
 #'
+#' @import dplyr
+#'
 #' @export
 check_duplicates <- function(data_df,
                              group_by_vars = NULL,
@@ -37,12 +39,12 @@ check_duplicates <- function(data_df,
   # Capture multiple rows per grouped variables
   mult_df <- data.frame()
   if (!is.null(group_by_vars)) {
-    mult_df <- data_df %>%
-      dplyr::ungroup() %>%
-      dplyr::distinct() %>%
-      dplyr::group_by(across(all_of(group_by_vars))) %>%
-      dplyr::filter(dplyr::n() > 1) %>%
-      dplyr::arrange(across(all_of(group_by_vars)))
+    mult_df <- data_df |>
+      dplyr::ungroup() |>
+      dplyr::distinct() |>
+      dplyr::group_by(dplyr::across(dplyr::all_of(group_by_vars))) |>
+      dplyr::filter(dplyr::n() > 1) |>
+      dplyr::arrange(dplyr::across(dplyr::all_of(group_by_vars)))
   }
 
   union_df <- dplyr::bind_rows(dup_df, mult_df)
