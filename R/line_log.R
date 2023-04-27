29b5ed188111868df1e58a02620d41d5e198f95e
PorwalS <PorwalS@mskcc.org>
2023-04-27 09:53:05 -0400

    Update line_log, remove unnecessary bits

--- R/line_log.R
+++ R/line_log.R
@@ -1,28 +1,44 @@
-
-
+#' Log Information About Comments and TODOs
+#'
+#' This function logs information about comments and TODOs added in a file, storing metadata
+#' in a specified data path.
+#'
+#' @param type A character string representing the type of annotation, either "COMMENT" or "TODO".
+#' @param item A character string or object containing the comment or TODO line(s).
+#' @param current_file A character string representing the name of the current file being edited. Default is the current file.
+#' @param data_path A character string representing the path to the directory where log files should be saved. Default is a "secure_data" subfolder in the working directory.
+#'
+#' @return NULL. This function is called for its side effect of saving log files.
+#' @export
+#'
+#' @examples
+#' # Log a comment
+#' line_log("COMMENT", "This is a comment.")
+#'
+#' # Log a TODO with a custom current_file and data_path
+#' line_log("TODO", "This is a TODO.", "my_file.R", "custom_data_path")
 line_log <- function(type,
                      item,
-                     editor_context = get_current_file(),
+                     current_file = get_current_file(),
                      data_path = file.path(getwd(), "secure_data", PROJECT_DICT$data_date)) {
 
   type <- stringr::str_to_upper(type)
 
-  time <- trunc(as.numeric(format(Sys.time(), "%H%M%OS3")))
+  acceptable_types = c('COMMENT', 'TODO')
 
-  if (type == "COMMENT") {
-
-    saveRDS(list(log = item,
-                 file = editor_context,
-                 time = time),
-            glue::glue("{data_path}/Metadata/Line Logs/Comments/{type}_{time}.rds"))
-
-  } else if (type == "TODO") {
-
-    saveRDS(list(log = item,
-                 file = editor_context,
-                 time = time),
-            glue::glue("{data_path}/Metadata/Line Logs/Todos/{type}_{time}.rds"))
-
+  if (!(type %in% acceptable_types)){
+    stop('Unacceptable type, has to be either COMMENT or TODO', call. = TRUE)
   }
 
+  time <- trunc(as.numeric(format(Sys.time(), "%H%M%OS3")))
+
+  saveRDS(list(log = item,
+               file = current_file,
+               time = time),
+          glue::glue("{data_path}/Metadata/Line Logs/{type}_{time}.rds"))
+
+  saveRDS(list(log = item,
+               file = current_file,
+               time = time),
+          glue::glue("{data_path}/Metadata/Line Logs/{type}_{time}.rds"))
 }
