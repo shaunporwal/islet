732cf12de41cb8f0ca865ec3af01f71788dbdfcc
PorwalS <PorwalS@mskcc.org>
2023-10-17 11:56:39 -0400

    Add roxygen skeleton to read_raw_data

--- R/read_raw_data.R
+++ R/read_raw_data.R
@@ -1,3 +1,20 @@
+#' Read and Clean Raw Data
+#'
+#' This function reads a CSV file and performs several cleaning steps:
+#' 1. Converts column names to uppercase.
+#' 2. Attempts to convert character columns to date format.
+#' 3. Converts character columns to uppercase.
+#'
+#' @param file A string specifying the path to the CSV file to be read.
+#'
+#' @return A data frame with cleaned column names and data.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   # Assuming 'data.csv' contains appropriate data
+#'   cleaned_data <- read_raw_data("data.csv")
+#' }
 read_raw_data <- function(file){
   df_raw_data <- read.csv(file = file, stringsAsFactors = FALSE) %>%
     janitor::clean_names(case = "all_caps") %>%
