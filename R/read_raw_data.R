fddac57e71e25c28ca887e5686553c79f8025348
PorwalS <PorwalS@mskcc.org>
2023-10-24 11:12:05 -0400

    document and check, fixing imports

--- R/read_raw_data.R
+++ R/read_raw_data.R
@@ -1,40 +1,3 @@
-#' #' Read and Clean Raw Data from a CSV File
-#' #'
-#' #' @param file A string specifying the full path to the CSV file.
-#' #' @return A data frame with cleaned column names and data.
-#' #' @export
-#' #'
-#' #' @examples
-#' #' \dontrun{
-#' #'   cleaned_data <- read_raw_data("data.csv")
-#' #' }
-#' #' @importFrom utils read.csv
-#' #' @importFrom janitor clean_names
-#' #' @importFrom dplyr mutate across where
-#' read_raw_data <- function(file) {
-#'   df_raw_data <- read.csv(file = file, stringsAsFactors = FALSE) |>
-#'     janitor::clean_names(case = "all_caps") |>
-#'     dplyr::mutate(across(where(is.character), function(col) {
-#'       tryCatch({
-#'         converted <- as.Date(col)
-#'         if (all(is.na(converted))) {
-#'           if (all(stringi::stri_enc_isutf8(col))) {
-#'             return(toupper(iconv(col, to = "UTF-8")))
-#'           } else {
-#'             return(col)
-#'           }
-#'         } else {
-#'           return(converted)
-#'         }
-#'       }, error = function(e) {
-#'         return(toupper(iconv(col, to = "UTF-8")))
-#'       })
-#'     }))
-#'
-#'   return(df_raw_data)
-#' }
-
-
 #' Read and Clean Raw Data from a CSV or RDS File
 #'
 #' @param file A string specifying the full path to the CSV or RDS file.
@@ -46,10 +9,12 @@
 #'   cleaned_data_csv <- read_raw_data("data.csv")
 #'   cleaned_data_rds <- read_raw_data("data.rds")
 #' }
-#' @importFrom utils read.csv readRDS
+#' @importFrom utils read.csv
 #' @importFrom tools file_ext
 #' @importFrom janitor clean_names
 #' @importFrom dplyr mutate across where
+#' @importFrom stringi stri_enc_isutf8
+
 read_raw_data <- function(file) {
   file_extension <- tools::file_ext(file)
 
